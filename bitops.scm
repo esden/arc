7127a14a76c96a4048d7e8ad9bd50ca5d087bc02
Piotr Esden-Tempski <piotr@esden.net>
2008-07-20 18:13:49 +0200

    Added C style bitops

---
+++ bitops.scm
@@ -0,0 +1,22 @@
+; Operations on bits.
+; C style
+;
+; NOTES:
+; We can not use ~ that is why bnot has been defined
+; hopefully it can be resolved in the future
+
+(module bitops mzscheme
+
+; so we can use xdef
+(require ac)
+
+(provide (all-defined))
+
+(xdef '& bitwise-and)
+(xdef '\| bitwise-ior)
+(xdef 'bnot bitwise-not)
+(xdef '^ bitwise-xor)
+(xdef '<< arithmetic-shift)
+(xdef '>> (lambda (x y) (arithmetic-shift x (- y))))
+
+)
